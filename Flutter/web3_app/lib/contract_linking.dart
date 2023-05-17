c97588a9a7a96b1ca3b2e24b7814bab421a39070
A-Hashemian <amin@hashemian.tech>
2023-05-17 23:58:34 +0300

    contract_dart

--- Flutter/web3_app/lib/contract_linking.dart
+++ Flutter/web3_app/lib/contract_linking.dart
@@ -1,7 +1,9 @@
 class Contractlinking{
 
+  final String rpcUrl ="http://127.0.0.1:8545";
+  final String wsUrl ="ws://127.0.0.1:8545";
+  final String privateUrl ="a622d80d9181b016de1f3667d52c0abde8c03d3c5386540eddfb837ac7227b80";
 
 
 
-    
 }
