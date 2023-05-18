e57e476080f479e5dbb89276c28f2579089ca4d9
A-Hashemian <amin@hashemian.tech>
2023-05-18 23:37:48 +0300

    dart_contract

--- Flutter/web3_app/lib/contract_linking.dart
+++ Flutter/web3_app/lib/contract_linking.dart
@@ -1,9 +1,11 @@
+import 'package:web3dart/web3dart.dart';
+
 class Contractlinking{
 
   final String rpcUrl ="http://127.0.0.1:8545";
   final String wsUrl ="ws://127.0.0.1:8545";
   final String privateUrl ="a622d80d9181b016de1f3667d52c0abde8c03d3c5386540eddfb837ac7227b80";
 
-
+Web3Client Web3client;
 
 }
