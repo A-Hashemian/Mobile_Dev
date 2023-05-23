24ffdbd469c36123e6fa6988b56a1c1a906e1467
A-Hashemian <amin@hashemian.tech>
2023-05-23 23:39:11 +0300

    update

--- Flutter/web3_app/lib/contract_linking.dart
+++ Flutter/web3_app/lib/contract_linking.dart
@@ -32,7 +32,7 @@ setup() async {
 
   _web3client=Web3Client(_rpcUrl,Client().socketConnector:(){
 
-    return IOWebSocketChannel.connect(url: _wsUrl)
+    return IOWebSocketChannel.connect(url: _wsUrl).cast();
   });
 
 }
