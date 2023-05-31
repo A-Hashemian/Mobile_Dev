60306f52ca4c938d684544e3e30606bf31a2ce31
A-Hashemian <amin@hashemian.tech>
2023-05-31 23:37:37 +0300

    update

--- Flutter/openai_image/lib/themes/my_themes.dart
+++ Flutter/openai_image/lib/themes/my_themes.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class MyThemes{
 
 
-static ThemeData themeData ({required bool isDarkTheme, required BuildContext context} )
+static ThemeData themeData ({required bool isDarkTheme, required BuildContext context} ) {
+
+  return new ThemeData;
+}
 
   
 }
